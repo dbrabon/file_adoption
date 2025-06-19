d09fe406fb9fb1f45967b18231d84ba8c2620965
Daniel Boone Brabon <dbrabon@sandiego.gov>
2025-06-19 10:14:59 -0700

    Improve media usage detection

--- src/FileScanner.php
+++ src/FileScanner.php
@@ -362,7 +362,11 @@ class FileScanner {
         $query->join('file_usage', 'fu', 'fu.fid = fm.fid');
         $query->addField('fu', 'fid');
         $query->condition('fm.uri', $uri);
-        $query->condition('fu.module', 'media');
+        $query->condition(
+            $query->orConditionGroup()
+                ->condition('fu.module', 'media')
+                ->condition('fu.type', 'media')
+        );
         $query->range(0, 1);
         return (bool) $query->execute()->fetchField();
     }
