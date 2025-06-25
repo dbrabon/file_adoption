9964b3c2e04c1f2f7a3129981d71bef394579e53
Daniel Boone Brabon <dbrabon@sandiego.gov>
2025-06-25 03:13:02 -0700

    Update PHPUnit config for version 10

---
+++ tests/BehatMinkStub.php
@@ -0,0 +1,5 @@
+<?php
+namespace Behat\Mink\Element;
+if (!class_exists('Behat\\Mink\\Element\\TraversableElement')) {
+    class TraversableElement {}
+}
---
+++ tests/ProphecyStub.php
@@ -0,0 +1,5 @@
+<?php
+namespace Prophecy\PhpUnit;
+if (!trait_exists('Prophecy\\PhpUnit\\ProphecyTrait')) {
+    trait ProphecyTrait {}
+}
